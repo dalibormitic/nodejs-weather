3861d42bbb9b9cac391722d3627854068ddeb114
Dalibor <dalibor.mitic997@gmail.com>
2020-07-21 17:45:52 +0200

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js');
-
 const form = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
